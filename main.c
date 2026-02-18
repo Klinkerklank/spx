c8df7cd0bb0c5651d019627597091e64d630276f
Klinkerklank <dennisoptroodt@gmail.com>
2026-02-18 16:42:23 +0100

    Added ADRS code, and a small example for printing the ADRS

--- main.c
+++ main.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <assert.h>
 
-uint64_t spx(void); // declaration of Jasmin function
+// command to run this file:
+// clear; cd ~/spx; jasminc -o spx.s spx.jazz; echo '.section .note.GNU-stack,"",@progbits' >> spx.s; gcc spx.s main.c -o test -no-pie; ./test
+
+uint8_t spx(uint64_t adrs_addr); // declaration of the SPHINCS+ Jasmin function
 
 int main() {
-    uint64_t r = spx();
-    printf("0x%016lx\n", r); // print an output of 64 bytes in hexadecimal
+    uint32_t adrs[8] = {0};
+
+    uint8_t r = spx((uint64_t)adrs);
+    assert(r == 0);
+
+    printf("ADRS:\n");
+    for (int i = 0; i < 8; i++) {
+        printf("%08X", adrs[i]);
+        if (i == 0) {
+            printf(" (layer addr)\n");
+        } else if (i == 1 || i == 2 || i == 3) {
+            printf(" (tree addr)\n");
+        } else if (i == 4) {
+            printf(" (type)\n");
+        } else if (i == 5) {
+            printf(" (key pair addr)\n");
+        } else if (i == 6) {
+            printf(" (chain addr / tree height)\n");
+        } else if (i == 7) {
+            printf(" (hash addr / tree idx)\n");
+        }
+    }
+
     return 0;
 }
