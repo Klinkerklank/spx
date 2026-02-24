02b13e5bf674738e2130af1d07aca42c8bd3bc2d
Klinkerklank <dennisoptroodt@gmail.com>
2026-02-24 12:01:27 +0100

    Added file content signatures in comments. Rewrote ADRS test to be in its own export function.

--- test.c
+++ test.c
@@ -3,14 +3,14 @@
 #include <assert.h>
 
 // command to run this file:
-// clear; cd ~/spx; jasminc -o spx.s spx.jazz; echo '.section .note.GNU-stack,"",@progbits' >> spx.s; gcc spx.s main.c -o test -no-pie; ./test
+// clear; cd ~/spx; jasminc -o spx.s spx.jazz; echo '.section .note.GNU-stack,"",@progbits' >> spx.s; gcc spx.s test.c -o test -no-pie; ./test
 
-uint8_t spx(uint64_t adrs_addr); // declaration of the SPHINCS+ Jasmin function
+uint8_t test_adrs(uint64_t adrs_addr); // declaration of the SPHINCS+ Jasmin function
 
 int main() {
     uint32_t adrs[8] = {0};
 
-    uint8_t r = spx((uint64_t)adrs);
+    uint8_t r = test_adrs((uint64_t)adrs);
     assert(r == 0);
 
     printf("ADRS:\n");
