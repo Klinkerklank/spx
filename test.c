0bd311a71e1c34293506395419713a6d69787592
Klinkerklank <dennisoptroodt@gmail.com>
2026-02-26 12:53:15 +0100

    added test_sha256 to test how to run the sha256 Jasmin implementation, which also produces correct output now

--- test.c
+++ test.c
@@ -5,9 +5,27 @@
 // command to run this file:
 // clear; cd ~/spx; jasminc -o spx.s spx.jazz; echo '.section .note.GNU-stack,"",@progbits' >> spx.s; gcc spx.s test.c -o test -no-pie; ./test
 
-uint8_t test_adrs(uint64_t adrs_addr); // declaration of the SPHINCS+ Jasmin function
+uint8_t test_adrs(uint64_t adrs_addr); // declaration of the ADRS testing function
+uint8_t test_sha256(uint64_t digest);  // declaration of the SHA256 testing function
 
-int main() {
+int sha256() {
+    uint8_t digest[32] = {0};
+    uint8_t r = test_sha256((uint64_t)digest);
+    assert(r == 0);
+
+    printf("SHA256:\n");
+    for (int i = 0; i < 32; i++) {
+        printf("%02x", digest[i]);
+        if (i%2 == 1) {
+        printf(" ");
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
+
+int adrs() {
     uint32_t adrs[8] = {0};
 
     uint8_t r = test_adrs((uint64_t)adrs);
@@ -30,6 +48,13 @@ int main() {
             printf(" (hash addr / tree idx)\n");
         }
     }
+
+    return 0;
+}
+
+int main() {
+    sha256();
+    // adrs();
 
     return 0;
 }
