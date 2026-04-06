743ff48e2c06d610aac17bc62005eb89b96a4d75
Klinkerklank <dennisoptroodt@gmail.com>
2026-04-06 16:15:01 +0200

    adapted implementations of hash functions to all provide a test result error-free (though not necessarily correct)

--- test.c
+++ test.c
@@ -5,35 +5,23 @@
 // command to run this file:
 // clear; cd ~/spx; jasminc -o spx.s spx.jazz; echo '.section .note.GNU-stack,"",@progbits' >> spx.s; gcc spx.s test.c -o test -no-pie; ./test
 
-uint8_t test_adrs(uint64_t adrs_addr, uint64_t adrsc_addr); // declaration of the ADRS testing function
-uint8_t test_sha256(uint64_t digest); // declaration of the SHA256 testing function
+uint8_t test_ADRS(uint64_t adrs_addr, uint64_t adrsc_addr);
+uint8_t test_H_msg(uint64_t digest, uint64_t m_addr, uint64_t m_len);
+uint8_t test_PRF_msg(uint64_t digest, uint64_t m_addr, uint64_t m_len);
+uint8_t test_F(uint64_t digest);
+uint8_t test_H(uint64_t digest);
+uint8_t test_T_len(uint64_t digest);
+uint8_t test_T_k(uint64_t digest);
 
-int sha256() {
-    uint8_t digest[16] = {0};
-    uint8_t r = test_sha256((uint64_t)digest);
-    assert(r == 0);
-
-    printf("SHA256:\n");
-    for (int i = 0; i < 16; i++) {
-        printf("%02x", digest[i]);
-        if (i%2 == 1) {
-        printf(" ");
-        }
-    }
-    printf("\n");
-
-    return 0;
-}
-
-int adrs() {
+int ADRS() {
     uint32_t adrs[8] = {0};
     uint8_t adrsc[22] = {0};
 
-    uint8_t r = test_adrs((uint64_t)adrs, (uint64_t)adrsc);
+    uint8_t r = test_ADRS((uint64_t)adrs, (uint64_t)adrsc);
     assert(r == 0);
 
     printf("ADRS:\n");
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < sizeof(adrs); i++) {
         printf("%08X", adrs[i]);
         if (i == 0) {
             printf(" (layer addr)\n");
@@ -52,7 +40,7 @@ int adrs() {
 
     printf("\n");
     printf("ADRS^c:\n");
-    for (int i = 0; i < 22; i++) {
+    for (int i = 0; i < sizeof(adrsc); i++) {
         if (i == 0) {
             printf("      %01X", adrsc[i]);
             printf(" (layer addr)\n");
@@ -72,9 +60,141 @@ int adrs() {
     return 0;
 }
 
+int H_msg() {
+    uint8_t digest[34] = {0};
+
+    uint8_t input[] = {
+        0x61, 0x62, 0x63
+    };
+    
+    uint8_t r = test_H_msg((uint64_t)digest,(uint64_t)input,(uint64_t)sizeof(input));
+    assert(r == 0);
+
+
+    printf("H_msg(");
+    for (int i = 0; i < sizeof(input); i++) {
+        printf("%02X", input[i]);
+        if (i < sizeof(input)-1) {
+            printf(",");
+        }
+    }
+    printf("):\n");
+    for (int i = 0; i < sizeof(digest); i++) {
+        printf("%02X", digest[i]);
+        if (i%2 == 1) {
+        printf(" ");
+        }
+    }
+    printf("\n\n");
+
+    return 0;
+}
+
+int PRF_msg() {
+    uint8_t digest[16] = {0};
+
+    uint8_t input[] = {
+        0x61, 0x62, 0x63
+    };
+    
+    uint8_t r = test_PRF_msg((uint64_t)digest,(uint64_t)input,(uint64_t)sizeof(input));
+    assert(r == 0);
+
+    printf("PRF_msg(");
+    for (int i = 0; i < sizeof(input); i++) {
+        printf("%02X", input[i]);
+        if (i < sizeof(input)-1) {
+            printf(",");
+        }
+    }
+    printf("):\n");
+    for (int i = 0; i < sizeof(digest); i++) {
+        printf("%02X", digest[i]);
+        if (i%2 == 1) {
+        printf(" ");
+        }
+    }
+    printf("\n\n");
+
+    return 0;
+}
+
+int F() {
+    uint8_t digest[16] = {0};
+    uint8_t r = test_F((uint64_t)digest);
+    assert(r == 0);
+
+    printf("F:\n");
+    for (int i = 0; i < sizeof(digest); i++) {
+        printf("%02X", digest[i]);
+        if (i%2 == 1) {
+        printf(" ");
+        }
+    }
+    printf("\n\n");
+
+    return 0;
+}
+
+int H() {
+    uint8_t digest[16] = {0};
+    uint8_t r = test_H((uint64_t)digest);
+    assert(r == 0);
+
+    printf("H:\n");
+    for (int i = 0; i < sizeof(digest); i++) {
+        printf("%02X", digest[i]);
+        if (i%2 == 1) {
+        printf(" ");
+        }
+    }
+    printf("\n\n");
+
+    return 0;
+}
+
+int T_len() {
+    uint8_t digest[16] = {0};
+    uint8_t r = test_T_len((uint64_t)digest);
+    assert(r == 0);
+
+    printf("T_len:\n");
+    for (int i = 0; i < sizeof(digest); i++) {
+        printf("%02X", digest[i]);
+        if (i%2 == 1) {
+        printf(" ");
+        }
+    }
+    printf("\n\n");
+
+    return 0;
+}
+
+int T_k() {
+    uint8_t digest[16] = {0};
+    uint8_t r = test_T_k((uint64_t)digest);
+    assert(r == 0);
+
+    printf("T_k:\n");
+    for (int i = 0; i < sizeof(digest); i++) {
+        printf("%02X", digest[i]);
+        if (i%2 == 1) {
+        printf(" ");
+        }
+    }
+    printf("\n\n");
+
+    return 0;
+}
+
 int main() {
-    adrs();
-    // sha256();
+    // ADRS();
+    H_msg();
+    PRF_msg();
+    F();
+    H();
+    T_len();
+    T_k();
 
     return 0;
 }
